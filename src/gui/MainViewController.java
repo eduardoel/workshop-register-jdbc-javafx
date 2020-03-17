afdb64674c42fc08f3eca6f0ed46f71cadb97c00
Eduardo Elly <eduardo_elly@hotmail.com>
2020-03-17 18:30:30 -0300

    Main view controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,27 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
-/**
- * FXML Controller class
- *
- * @author eduar
- */
 public class MainViewController implements Initializable {
+    
+    @FXML //esse três são os atributos de item de menu
+    private MenuItem menuItemSeller;//(1)SELLER - VENDEDOR
+    @FXML
+    private MenuItem menuItemDepartment;//(2)DEPARTMENT - DEPARTAMENTO
+    @FXML
+    private MenuItem menuItemAbout;//(3)HELP - AJUDA
+    
+    @FXML //Metodo para tratar os eventos do menu
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAcrion");
+    }
+    
+    @FXML //Metodo para tratar os eventos do menu
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAcrion");
+    }
+    
+    @FXML //Metodo para tratar os eventos do menu
+    public void onMenuItemAboutAcrion() {
+        System.out.println("onMenuItemAboutAcrion");
+    }
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
